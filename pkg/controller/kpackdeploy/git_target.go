08ce826c2c9d05d01052f212681b69071c74e248
cpage-pivotal <cpage@pivotal.io>
2020-07-05 10:55:55 -0500

    Stubbed out writeToGitTarget method

---
+++ pkg/controller/kpackdeploy/git_target.go
@@ -0,0 +1,7 @@
+package kpackdeploy
+
+import kpackdeployv1alpha1 "github.com/cpage-pivotal/kpack-deploy-operator/pkg/apis/kpackdeploy/v1alpha1"
+
+func writeToGitTarget(latestImage string, git kpackdeployv1alpha1.Git) {
+
+}
--- pkg/controller/kpackdeploy/kpackdeploy_controller.go
+++ pkg/controller/kpackdeploy/kpackdeploy_controller.go
@@ -53,16 +53,6 @@ func add(mgr manager.Manager, r reconcile.Reconciler) error {
 		return err
 	}
 
-	// TODO(user): Modify this to be the types you create that are owned by the primary resource
-	// Watch for changes to secondary resource Pods and requeue the owner KpackDeploy
-	//err = c.Watch(&source.Kind{Type: &corev1.Pod{}}, &handler.EnqueueRequestForOwner{
-	//	IsController: true,
-	//	OwnerType:    &kpackdeployv1alpha1.KpackDeploy{},
-	//})
-	//if err != nil {
-	//	return err
-	//}
-
 	return nil
 }
 
@@ -110,55 +100,7 @@ func (r *ReconcileKpackDeploy) Reconcile(request reconcile.Request) (reconcile.R
 	}
 
 	reqLogger.Info("Found Image!", "LatestImage", foundBuild.Status.LatestImage)
+	writeToGitTarget(foundBuild.Status.LatestImage, instance.Spec.Target.Git)
 
-	// Define a new Pod object
-	//pod := newPodForCR(instance)
-	//
-	//// Set KpackDeploy instance as the owner and controller
-	//if err := controllerutil.SetControllerReference(instance, pod, r.scheme); err != nil {
-	//	return reconcile.Result{}, err
-	//}
-	//
-	//// Check if this Pod already exists
-	//found := &corev1.Pod{}
-	//err = r.client.Get(context.TODO(), types.NamespacedName{Name: pod.Name, Namespace: pod.Namespace}, found)
-	//if err != nil && errors.IsNotFound(err) {
-	//	reqLogger.Info("Creating a new Pod", "Pod.Namespace", pod.Namespace, "Pod.Name", pod.Name)
-	//	err = r.client.Create(context.TODO(), pod)
-	//	if err != nil {
-	//		return reconcile.Result{}, err
-	//	}
-	//
-	//	// Pod created successfully - don't requeue
-	//	return reconcile.Result{}, nil
-	//} else if err != nil {
-	//	return reconcile.Result{}, err
-	//}
-	//
-	//// Pod already exists - don't requeue
-	//reqLogger.Info("Skip reconcile: Pod already exists", "Pod.Namespace", found.Namespace, "Pod.Name", found.Name)
 	return reconcile.Result{}, nil
 }
-
-// newPodForCR returns a busybox pod with the same name/namespace as the cr
-//func newPodForCR(cr *kpackdeployv1alpha1.KpackDeploy) *corev1.Pod {
-//	labels := map[string]string{
-//		"app": cr.Name,
-//	}
-//	return &corev1.Pod{
-//		ObjectMeta: metav1.ObjectMeta{
-//			Name:      cr.Name + "-pod",
-//			Namespace: cr.Namespace,
-//			Labels:    labels,
-//		},
-//		Spec: corev1.PodSpec{
-//			Containers: []corev1.Container{
-//				{
-//					Name:    "busybox",
-//					Image:   "busybox",
-//					Command: []string{"sleep", "3600"},
-//				},
-//			},
-//		},
-//	}
-//}
